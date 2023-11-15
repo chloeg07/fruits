a257d23d97f9bd85ab9c6791e539fef49071fc9d
Chloe <144443749+chloeg07@users.noreply.github.com>
2023-11-15 12:07:15 +0000

    Add files via upload

---
+++ banana.py
@@ -0,0 +1,10 @@
+thislist = ["apple", "banana", "cherry", "orange", "kiwi", "melon", "mango"]
+print(thislist[2:5])#returns items at index 2,3 and 4
+thislist = ["apple", "banana", "cherry", "orange", "kiwi", "melon", "mango"]
+print(thislist[:5])#print everything from the start to position 5
+thislist = ["apple", "banana", "cherry", "orange", "kiwi", "melon", "mango"]
+print(thislist[2:]) # take everything from position 2 to the end
+thislist = ["apple", "banana", "cherry", "orange", "kiwi", "melon", "mango"]
+print(thislist[:]) # print the entire list
+thislist = ["apple", "banana", "cherry", "orange", "kiwi", "melon", "mango"]
+print(thislist[-4:-1]) #print from orange up to but not including mang
